47cc2d87acd00ab04a2671895ea5d50b5cea25eb
AlessioBucheri <157613253+AlessioBucheri@users.noreply.github.com>
2024-02-29 20:02:56 +0100

    edit css and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 let count = 0;
+let livello1 = 1;
 
 document.getElementById("decreaseBtn").onclick = function(){
     count-=1;
@@ -16,12 +17,13 @@ document.getElementById("increaseBtn").onclick = function(){
 }
 
 document.getElementById("confirm").onclick = function(){
-    if (count === 5){
+    if (count === 5 && livello1 == 1){
         alert("Corretto, passiamo al livello successivo!")
         document.getElementById('lvl-1-A').src = './assets/images/lvl-2-A.png';
         document.getElementById('lvl-1-B').src = './assets/images/lvl-2-B.png';
+        ++livello1;
     }
-    else if (count === 7){
+    else if (count === 7 && livello1 == 2){
         alert("Corretto, passiamo all'ultimo livello!")
         document.getElementById('lvl-1-A').src = './assets/images/lvl-3-A.png';
         document.getElementById('lvl-1-B').src = './assets/images/lvl-3-B.png';
